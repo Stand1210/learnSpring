67bc5ac85308bfea253ca3cebb9cb03116abe671
宋珂琦 <stand@songkeqideMacBook-Pro.local>
2017-02-17 14:55:12 +0800

    bean作用域

---
+++ BeanScope/src/main/com/stand/BeanScope.java
@@ -0,0 +1,12 @@
+package main.com.stand;
+
+/**
+ * Created by stand on 2017/2/17.
+ */
+public class BeanScope {
+
+    public void say() {
+        System.out.println("BeanScope say:" + this.hashCode());
+    }
+
+}
---
+++ BeanScope/src/test/com/stand/Test.java
@@ -0,0 +1,31 @@
+package test.com.stand;
+
+import main.com.stand.BeanScope;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by stand on 2017/2/17.
+ */
+@ContextConfiguration("classpath:spring-beanScope.xml")
+public class Test extends AbstractJUnit4SpringContextTests{
+    @Resource(name = "BeanScope")
+    private BeanScope beanScope;
+
+    @org.junit.Test
+    public void test1() {
+
+        beanScope.say();
+        beanScope.say();
+
+    }
+
+    @org.junit.Test
+    public void test2()
+    {
+        beanScope.say();
+    }
+
+}
